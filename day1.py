787142ccd675e8b936ce1053eefd1fe1c6a793d7
shaunnorris <shaunnorris@gmail.com>
2023-12-02 07:28:52 +0000

    day 1 and 2 solutions

---
+++ day1.py
@@ -0,0 +1,155 @@
+import pytest
+
+def read_file_lines(filepath):
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines]
+
+def test_read_file_lines(tmp_path):
+    # Create a temporary file and write some content to it
+    test_file = tmp_path / "test_file.txt"
+    with open(test_file, 'w') as f:
+        f.write("Line 1\nLine 2\nLine 3")
+
+    # Use the function to read the file's content
+    result = read_file_lines(test_file)
+
+    # Assert that the result is a list of lines as expected
+    assert result == ["Line 1", "Line 2", "Line 3"]
+ 
+
+def test_extract_digits():
+    assert extract_digits("1abc2") == (12)
+    assert extract_digits("pqr3stu8vwx") == (38)
+    assert extract_digits("a1b2c3d4e5f") == (15)
+    assert extract_digits("treb7uchet") == (77)
+    assert extract_digits("abc123xyz") == (13)  # Including previous tests for thoroughness
+    assert extract_digits("1234") == (14)
+    assert extract_digits("a1b2c3") == (13)
+    assert extract_digits("7abc") == (77)
+
+def extract_digits_old(input_string):
+    """Extract the first and last digits from a string"""
+    digits = [char for char in input_string if char.isdigit()]
+    if len(digits) == 0:
+        return None
+    else:
+        return (int(digits[0]) * 10) + int(digits[-1])
+
+def extract_digits(s):
+    def find_first_digit(ss):
+        for char in ss:
+            if char.isdigit():
+                return int(char)
+    
+    first_digit = find_first_digit(s)
+    last_digit = find_first_digit(reversed(s))
+
+    
+    return first_digit*10 + last_digit
+
+
+def solve_part1(input):
+    puzzle_sum = 0
+    for line in input:
+        puzzle_sum += extract_digits(line)
+    return puzzle_sum
+
+def test_solve_part1():
+    assert solve_part1(["1abc2","pqr3stu8vwx","a1b2c3d4e5f","treb7uchet"]) == 142
+
+
+
+def replace_text_old(s):
+    number_map = {
+        "one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+        "six": "6", "seven": "7", "eight": "8", "nine": "9"
+    }
+
+    result = list(s)
+    i = 0
+    while i < len(result):
+        replaced = False
+        # Check for each spelled number in order of length
+        for length in range(5, 2, -1):  # Check 5, 4, 3 characters length
+            if i + length <= len(result):
+                substring = ''.join(result[i:i+length])
+                if substring in number_map:
+                    # Replace the first character of the found spelled word with the digit
+                    result[i] = number_map[substring]
+                    replaced = True
+                    break
+        i += 1  # Move forward by one position regardless of whether a replacement was made or not
+
+    return ''.join(result)
+
+        
+def test_parse_part2():
+    assert extract_digits_part2("abcone2threexyz") == (13)
+    assert extract_digits_part2("two1nine") == (29)
+    assert extract_digits_part2("sevenine") == (79)
+    assert extract_digits_part2("eighthree") == (83)
+    assert extract_digits_part2("eightwothree") == (83)
+    assert extract_digits_part2("xtwone3four") == (24)
+    assert extract_digits_part2("4nineeightseven2") == (42)
+    assert extract_digits_part2("zoneight234") == (14)
+    assert extract_digits_part2("7pqrstsixteen") == (76)
+    assert extract_digits_part2("gndxnlmnrmnk29qkfxfoursnnbvjtq") == (24)
+
+
+def extract_digits_part2(s):
+    def find_first_digit_part2(s,direction):
+
+        stack = []
+        if direction == "forward":
+            number_map = {
+                "one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+                "six": "6", "seven": "7", "eight": "8", "nine": "9"
+            }
+        elif direction == "backward":
+            number_map = {
+                "eno": "1", "owt": "2", "eerht": "3", "ruof": "4", "evif": "5",
+                "xis": "6", "neves": "7", "thgie": "8", "enin": "9"
+            }
+        for char in s:
+            stack.append(char)
+            if len(stack)  >=3:
+                for start in range(len(stack)-2):
+                    word = ''.join(stack[start:])
+                    if word in number_map:
+                        return int(number_map[word])
+            if len(stack) > 5:
+                stack.pop(0)    
+            if char.isdigit():
+                return int(char)
+        return None
+    
+    first_digit = find_first_digit_part2(s,'forward')
+    last_digit = find_first_digit_part2(reversed(s),'backward')
+    return first_digit*10 + last_digit
+
+def solve_part2(input):
+    puzzle_sum = 0
+    for line in input:
+        puzzle_sum += extract_digits_part2(line)
+    return puzzle_sum
+
+def test_solve_part2():
+    assert solve_part2(["two1nine",
+                        "eightwothree",
+                        "abcone2threexyz",
+                        "xtwone3four",
+                        "4nineeightseven2",
+                        "zoneight234",     
+                        "7pqrstsixteen"
+                        ]) == 281
+
+                     
+input = read_file_lines("day1-input.txt")
+part1 = solve_part1(input)
+print("Part 1:", part1)
+
+
+part2 = solve_part2(input)
+print("Part 2:", part2)
+
---
+++ day2.py
@@ -0,0 +1,98 @@
+import pytest
+import re
+
+test_gameset =['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', 
+               'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue', 
+               'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', 
+               'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', 
+               'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
+
+def read_file_lines(filepath):
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines]
+
+def test_read_file_lines(tmp_path):
+    test_file = "day2-test-input.txt"
+    result = read_file_lines(test_file)
+    assert result == test_gameset
+
+def test_parse_game():
+    assert parse_game("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green") == {'blue': 6, 'green': 2, 'id': 1, 'red': 4}
+    assert parse_game("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")  == {'id': 2, 'blue': 4, 'green': 3, 'red': 1}
+    assert parse_game("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")  == {'id': 3, 'green': 13, 'blue': 6, 'red': 20}
+    assert parse_game("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")  == {'id': 4, 'green': 3, 'red': 14, 'blue': 15}
+    assert parse_game("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")  == {'id': 5, 'red': 6, 'blue': 2, 'green': 3}
+
+def parse_game(game_string):   
+    game_cubes = {}
+    game_id, cubeinfo = game_string.split(":")
+    game_cubes['id'] = int(game_id.split()[-1]) 
+    
+    separators = ',|;'
+    cubes = re.split(separators, cubeinfo)
+    for cube in cubes:
+        qty, colour = cube.strip().split()
+        if colour in game_cubes:
+            if game_cubes[colour] < int(qty):
+                game_cubes[colour] = int(qty)
+        else:
+            game_cubes[colour] = int(qty)
+    return game_cubes
+
+def test_possibility():
+    test_possible_limits = {'blue': 14, 'green': 13, 'red': 12}
+    assert possible(test_gameset[0], test_possible_limits) == 1
+    assert possible(test_gameset[1], test_possible_limits) == 2
+    assert possible(test_gameset[2], test_possible_limits) == 0
+    assert possible(test_gameset[3], test_possible_limits) == 0
+    assert possible(test_gameset[4], test_possible_limits) == 5
+    
+def possible(game_string, possible_limits):
+    
+    game_cubes = parse_game(game_string)
+    for colour in game_cubes:
+        if colour != 'id':
+            if game_cubes[colour] > possible_limits[colour]:
+                return 0
+    return game_cubes['id']
+
+day2_input = read_file_lines("day2-input.txt")
+part1_limits = {'blue': 14, 'green': 13, 'red': 12}
+
+def test_solve_part1():
+    assert solve_part1(test_gameset) == 8
+
+def solve_part1(gamedata):
+    possible_games = []
+    for game in gamedata:
+        possible_games.append(possible(game, part1_limits))
+    return sum(possible_games)
+
+part1 = solve_part1(day2_input)
+print('Part 1:', part1)
+    
+
+def test_find_power():
+    assert find_power(test_gameset[0]) == 48
+    assert find_power(test_gameset[1]) == 12
+    assert find_power(test_gameset[2]) == 1560
+    assert find_power(test_gameset[3]) == 630
+    assert find_power(test_gameset[4]) == 36
+
+def find_power(game_string):
+    game_cubes = parse_game(game_string)
+    power = game_cubes['blue'] * game_cubes['red'] * game_cubes['green']
+    return power
+
+def test_solve_part2():
+    assert solve_part2(test_gameset) == 2286
+
+def solve_part2(gamedata):   
+    total_power = 0
+    for game in gamedata:
+        total_power += find_power(game)
+    return total_power
+
+part2 = solve_part2(day2_input)
+print('part 2:', part2)
